c87fcc1e3c7cc595c7194f2a0c01876a9da6f2bc
javeria2108 <javeriazh@gmail.com>
2023-05-29 15:53:01 +0500

    background fix

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import Pokedox from './Components/Pokedox';
 import styled from "styled-components";
-import img from './img.jpg'
+import img from './img/bg.gif';
 
+
+const Content=styled.div`
+position:fixed;
+top:0;
+left:0;
+min-width:100%;
+min-height:100%;
+border:1px solid #000;
+background-image: url(${img}) ;
+background-size: contain;
+`
 const Container=styled.div`
   display:flex;
   flex-direction:column;
   align-items:center;
   margin:30px 0 10px;
   font-family:Montserrat;
-  background-image: (url ${img});
+ 
 `
 const Header=styled.span`
   color:black;
@@ -20,12 +31,14 @@ function App() {
   return (
 
     <> 
+    <Content>
     <Container>
     <Header>
     Pokedox
   </Header>
   <Pokedox/>
   </Container>
+  </Content>
   </>
    
   );
